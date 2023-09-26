0b95474243030e9d68bd10649c28feba8d331600
Yara Lacerda <128132279+Yara-56@users.noreply.github.com>
2023-09-26 01:39:12 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,13 @@
 import br.com.alura.screenmatch.models.Movie;
+import br.com.alura.screenmatch.models.Serie;
 
 public class Main {
     public static void main(String[] args){
         Movie myMovie = new Movie();
         myMovie.setName("O poderoso chefão");
         myMovie.setRealizeYear(1970);
-        myMovie.setRealizeYear(180);
+        myMovie.setTimeInMinutes(180);
+        System.out.println("Duração do filme: "+ myMovie.getTimeInMinutes());
 
         myMovie.showDetais();
         myMovie.avalia(5);
@@ -13,5 +15,14 @@ public class Main {
         myMovie.avalia(5);
         System.out.println("Total de avaliações: " + myMovie.getTotalOfRate());
         System.out.println(myMovie.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setName("Lost");
+        lost.setRealizeYear(2000);
+        lost.showDetais();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        System.out.println("Duração do filme: "+ lost.getTimeInMinutes());
+
     }
 }
