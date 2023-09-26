ecd2647570cbfa5767e4bf8f34a5c2ddf081bd84
Yara Lacerda <128132279+Yara-56@users.noreply.github.com>
2023-09-26 01:38:47 -0300

    Create Titulo.java

---
+++ Titulo.java
@@ -0,0 +1,62 @@
+package br.com.alura.screenmatch.models;
+
+public class Titulo {
+    private String name;
+    private int realizeYear;
+    private boolean haveInPlan;
+    private double somaDasAvaliacoes;
+    private int totalOfRate;
+    private int timeInMinutes;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRealizeYear() {
+        return realizeYear;
+    }
+
+    public boolean isHaveInPlan() {
+        return haveInPlan;
+    }
+
+    public int getTimeInMinutes() {
+        return timeInMinutes;
+    }
+
+    public int getTotalOfRate(){
+
+        return totalOfRate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRealizeYear(int realizeYear) {
+        this.realizeYear = realizeYear;
+    }
+
+    public void setHaveInPlan(boolean haveInPlan) {
+        this.haveInPlan = haveInPlan;
+    }
+
+    public void setTimeInMinutes(int timeInMinutes) {
+        this.timeInMinutes = timeInMinutes;
+    }
+
+    public void showDetais(){
+        System.out.println("Nome do filme: " +name);
+        System.out.println("Ano de lançamento: " +realizeYear);
+    }
+
+    public void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalOfRate++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalOfRate;
+    }
+}
