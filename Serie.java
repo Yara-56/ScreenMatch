e1bd9f84693a2f08a945a27b478a8fcf93e96e0c
Yara Lacerda <128132279+Yara-56@users.noreply.github.com>
2023-09-26 01:38:12 -0300

    Update Serie.java

--- Serie.java
+++ Serie.java
@@ -1,1 +1,20 @@
+public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
 
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    public int getTimeInMinutes(){
+        return temporadas * episodiosPorTemporada * minutosPorEpisodio;
+    }
+}
